dfdf391526d6f786f7e2c9faeac7369fb6f07fbc
Rohan Shah <rohan.shah@bookshah.com>
2015-12-28 21:25:06 +1100

    Minor changes to formGroups tests

--- tests/testthat/test-formGroups.R
+++ tests/testthat/test-formGroups.R
@@ -2,7 +2,7 @@ context("Test formGroups")
 test_that("Check that formGroups requires lod entry if clusterBy is \"combined\" or \"lod\"",
 	{
 		f2Pedigree <- f2Pedigree(100)
-		map <- sim.map(len = rep(100, 2), n.mar = 100, anchor.tel=TRUE, include.x=FALSE, eq.spacing=TRUE)
+		map <- sim.map(len = rep(100, 2), n.mar = 10, anchor.tel=TRUE, include.x=FALSE, eq.spacing=TRUE)
 		cross <- simulateMPCross(map=map, pedigree=f2Pedigree, mapFunction = haldane)
 		rf <- estimateRF(cross)
 
@@ -19,7 +19,56 @@ test_that("Check that formGroups requires lod entry if clusterBy is \"combined\"
 	})
 test_that("Check that formGroups works for an f2 design",
 	{
+		f2Pedigree <- f2Pedigree(1000)
+		map <- sim.map(len = rep(100, 2), n.mar = 100, anchor.tel=TRUE, include.x=FALSE, eq.spacing=TRUE)
+		cross <- simulateMPCross(map=map, pedigree=f2Pedigree, mapFunction = haldane)
+		rf <- estimateRF(cross, keepLod = TRUE, keepLkhd = TRUE)
+
+		for(method in c("average", "complete", "single"))
+		{
+			for(clusterBy in c("combined", "theta", "lod"))
+			{
+				formGroups <- formGroups(mpcrossRF = rf, groups = 2, method = method, clusterBy = clusterBy)
+				formGroupsPreCluster <- formGroups(mpcrossRF = rf, groups = 2, method = method, clusterBy = clusterBy, preCluster = TRUE)
+				expect_identical(formGroups@lg, formGroupsPreCluster@lg)
+				expect_identical(length(formGroups@lg@allGroups), 2L)
+				chr1Group <- formGroups@lg@groups[names(map[[1]])[1]]
+				chr2Group <- formGroups@lg@groups[names(map[[2]])[1]]
+				expectedGroups <- c(rep(chr1Group, 100), rep(chr2Group, 100))
+				names(expectedGroups) <- markers(cross)
+
+				expect_identical(formGroups@lg@groups, expectedGroups)
+			}
+		}
+	})
+test_that("Check that formGroups works for an f2 design with small chromosomes",
+	{
 		f2Pedigree <- f2Pedigree(10000)
+		map <- sim.map(len = rep(5, 2), n.mar = 100, anchor.tel=TRUE, include.x=FALSE, eq.spacing=TRUE)
+		cross <- simulateMPCross(map=map, pedigree=f2Pedigree, mapFunction = haldane)
+		rf <- estimateRF(cross, keepLod = TRUE, keepLkhd = TRUE)
+
+		for(method in c("average", "complete", "single"))
+		{
+			for(clusterBy in c("combined", "theta", "lod"))
+			{
+				formGroups <- formGroups(mpcrossRF = rf, groups = 2, method = method, clusterBy = clusterBy)
+				formGroupsPreCluster <- formGroups(mpcrossRF = rf, groups = 2, method = method, clusterBy = clusterBy, preCluster = TRUE)
+				expect_identical(formGroups@lg, formGroupsPreCluster@lg)
+				expect_identical(length(formGroups@lg@allGroups), 2L)
+				chr1Group <- formGroups@lg@groups[names(map[[1]])[1]]
+				chr2Group <- formGroups@lg@groups[names(map[[2]])[1]]
+				expectedGroups <- c(rep(chr1Group, 100), rep(chr2Group, 100))
+				names(expectedGroups) <- markers(cross)
+
+				expect_identical(formGroups@lg@groups, expectedGroups)
+			}
+		}
+	})
+
+test_that("Check that formGroups works for a ril design",
+	{
+		f2Pedigree <- rilPedigree(1000, selfingGenerations = 10)
 		map <- sim.map(len = rep(100, 2), n.mar = 100, anchor.tel=TRUE, include.x=FALSE, eq.spacing=TRUE)
 		cross <- simulateMPCross(map=map, pedigree=f2Pedigree, mapFunction = haldane)
 		rf <- estimateRF(cross, keepLod = TRUE, keepLkhd = TRUE)
