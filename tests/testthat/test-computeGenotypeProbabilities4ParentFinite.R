1bc41c5d7198eca8d2c5332a9a85c925739e84fa
Rohan Shah <rohan.shah@bookshah.com>
2016-08-19 19:04:39 +1000

    Added extra tests

--- tests/testthat/test-computeGenotypeProbabilities4ParentFinite.R
+++ tests/testthat/test-computeGenotypeProbabilities4ParentFinite.R
@@ -16,14 +16,18 @@ test_that("Test fully informative markers",
 			expect_true(all(genotypesFromProbabilities == result@geneticData[[1]]@finals))
 		}
 		map <- sim.map(len = c(100, 100), n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
-		pedigree1 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
-		pedigree2 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 1, nSeeds = 1)
-		pedigree3 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
-		pedigree4 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 1, nSeeds = 1)
-		pedigree1@selfing <- pedigree2@selfing <- pedigree3@selfing <- pedigree4@selfing <- "finite"
-		pedigrees <- list(pedigree1, pedigree2, pedigree3, pedigree4)
+		pedigrees <- list()
+		pedigrees[[1]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[2]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 1, nSeeds = 1)
+		pedigrees[[3]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[4]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 1, nSeeds = 1)
+		pedigrees[[5]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[6]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 1, nSeeds = 1)
+		pedigrees[[7]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[8]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 1, nSeeds = 1)
 		for(pedigree in pedigrees)
 		{
+			pedigree@selfing <- "finite"
 			testFunc(map, pedigree)
 		}
 	})
@@ -45,12 +49,14 @@ test_that("Test SNP markers, hets not called, and NA not assumed biased towards 
 			expect_true(sum(diag(table(genotypesFromProbabilities, cross@geneticData[[1]]@finals))) / length(genotypesFromProbabilities) > 0.7)
 		}
 		map <- sim.map(len = c(100, 100), n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
-		pedigree1 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
-		pedigree2 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
-		pedigree1@selfing <- pedigree2@selfing <- "finite"
-		pedigrees <- list(pedigree1, pedigree2)
+		pedigrees <- list()
+		pedigrees[[1]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[2]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[3]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[4]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
 		for(pedigree in pedigrees)
 		{
+			pedigree@selfing <- "finite"
 			testFunc(map, pedigree)
 		}
 	})
@@ -72,14 +78,18 @@ test_that("Test SNP markers, hets not called, and NA assumed biased towards hets
 			expect_true(sum(diag(table(genotypesFromProbabilities, cross@geneticData[[1]]@finals))) / length(genotypesFromProbabilities) > 0.85)
 		}
 		map <- sim.map(len = c(100, 100), n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
-		pedigree1 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
-		pedigree2 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 1, nSeeds = 1)
-		pedigree3 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
-		pedigree4 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 1, nSeeds = 1)
-		pedigree1@selfing <- pedigree2@selfing <- pedigree3@selfing <- pedigree4@selfing <- "finite"
-		pedigrees <- list(pedigree1, pedigree2, pedigree3, pedigree4)
+		pedigrees <- list()
+		pedigrees[[1]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[2]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 1, nSeeds = 1)
+		pedigrees[[3]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[4]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 1, nSeeds = 1)
+		pedigrees[[5]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[6]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 1, nSeeds = 1)
+		pedigrees[[7]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[8]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 1, nSeeds = 1)
 		for(pedigree in pedigrees)
 		{
+			pedigree@selfing <- "finite"
 			testFunc(map, pedigree)
 		}
 	})
@@ -101,14 +111,18 @@ test_that("Test SNP markers, with hets called",
 			expect_true(sum(diag(table(genotypesFromProbabilities, cross@geneticData[[1]]@finals))) / length(genotypesFromProbabilities) > 0.9)
 		}
 		map <- sim.map(len = c(100, 100), n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
-		pedigree1 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
-		pedigree2 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 1, nSeeds = 1)
-		pedigree3 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
-		pedigree4 <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 1, nSeeds = 1)
-		pedigree1@selfing <- pedigree2@selfing <- pedigree3@selfing <- pedigree4@selfing <- "finite"
-		pedigrees <- list(pedigree1, pedigree2, pedigree3, pedigree4)
+		pedigrees <- list()
+		pedigrees[[1]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[2]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 1, nSeeds = 1)
+		pedigrees[[3]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[4]] <- fourParentPedigreeRandomFunnels(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 1, nSeeds = 1)
+		pedigrees[[5]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[6]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 0, intercrossingGenerations = 1, nSeeds = 1)
+		pedigrees[[7]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 0, nSeeds = 1)
+		pedigrees[[8]] <- fourParentPedigreeSingleFunnel(initialPopulationSize=500, selfingGenerations = 2, intercrossingGenerations = 1, nSeeds = 1)
 		for(pedigree in pedigrees)
 		{
+			pedigree@selfing <- "finite"
 			testFunc(map, pedigree)
 		}
 	})
