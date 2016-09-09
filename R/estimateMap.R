95653a1f28f13a28814455ecdb9bc8d92bb3cbeb
Rohan Shah <rohan.shah@bookshah.com>
2016-09-09 15:33:13 +1000

    Made estimateMap more sophisticated, it now maps chunks seperately.
    Also added tests for more complicated functionality.

--- R/estimateMap.R
+++ R/estimateMap.R
@@ -1,8 +1,9 @@
 #' Computes map distances
 #' 
+#' @param maxMarkers The (approximate) number of markers for which distances are estimated simultaneously. 
 #' Use the non-linear least squares function to estimate a map
 #' @export
-estimateMap <- function(mpcrossLG, mapFunction = rfToHaldane, maxOffset = 1)
+estimateMap <- function(mpcrossLG, mapFunction = rfToHaldane, maxOffset = 1, maxMarkers = 2000, verbose=FALSE)
 {
 	isNewMpcrossLGArgument(mpcrossLG)
 	if (is.null(mpcrossLG@rf) && is.null(mpcrossLG@lg@imputedTheta))
@@ -12,36 +13,87 @@ estimateMap <- function(mpcrossLG, mapFunction = rfToHaldane, maxOffset = 1)
 	if(is.null(mpcrossLG@lg@imputedTheta))
 	{
 		cat("Imputing recombination fractions\n", sep="")
-		mpcrossLG <- impute(mpcrossLG, verbose=TRUE)
+		mpcrossLG <- impute(mpcrossLG, verbose=verbose)
+	}
+	if(maxOffset > maxMarkers / 8)
+	{
+		stop("Input maxOffset must be smaller than maxMarkers / 8")
 	}
 	map <- list()
 	for (group in mpcrossLG@lg@allGroups)
 	{
+		if(verbose) cat("Starting linkage group ", group, "\n", sep="")
 		rfData <- mpcrossLG@lg@imputedTheta[[as.character(group)]]
 		maxOffset <- min(maxOffset,length(rfData@markers)-1)
-		#Construct design matrix
-		#d <- designMat(length(object$map[[chr]])-1, maxOffset)
-		d <- .Call("generateDesignMatrix", length(rfData@markers)-1, maxOffset, PACKAGE="mpMap2")
-		indices <- matrix(nrow=maxOffset * length(rfData@markers) - maxOffset * (maxOffset + 1) / 2, ncol = 2)
-		counter <- 1
-		for(offset in 1:maxOffset)
+		originalMarkers <- length(rfData@markers)
+		if(originalMarkers < maxMarkers)
 		{
-			for(i in 1:(length(rfData@markers)-offset))
+			ranges <- cbind(1, originalMarkers)
+		}
+		else
+		{
+			starts <- as.integer(seq(1, originalMarkers+1, length.out = ceiling(originalMarkers / maxMarkers)+1))
+			ranges <- cbind(head(starts, -1), tail(starts, -1)-1)
+		}
+		resultsThisGroup <- list()
+		if(verbose) cat("Splitting into ", length(ranges), " subproblems\n", sep="")
+		for(row in 1:nrow(ranges))
+		{
+			start <- ranges[row, 1]
+			end <- ranges[row, 2]
+			if(verbose) 
 			{
-				indices[counter,] <- c(i+offset, i)
-				counter <- counter + 1
+				cat("Iteration ", row, " / ", nrow(ranges), "\n", sep="")
+			}
+			subsettedRF <- subset(rfData, markers = start:end)
+			resultsThisGroup[[row]] <- estimateMapInternal(subsettedRF = subsettedRF, maxOffset = maxOffset, verbose = verbose, mapFunction = mapFunction)
+		}
+		currentChr <- resultsThisGroup[[1]]
+		#Now to glue all these bits together. 
+		if(nrow(ranges) > 1)
+		{
+			for(row in 2:nrow(ranges))
+			{
+				#Get out the map for the last maxOffset+1 markers of the end of the last chunk, and the first maxOffset+1 markers of the next chunk. 
+				subsettedRF <- subset(rfData, markers = (ranges[row-1,2] - 2*maxOffset):(ranges[row,1] + 2*maxOffset))
+				overlap <- estimateMapInternal(subsettedRF, maxOffset, FALSE, mapFunction = mapFunction)
+				currentChr[(length(currentChr) - maxOffset):(length(currentChr))] <- overlap[(maxOffset+1):(2*maxOffset+1)] - overlap[maxOffset+1] + currentChr[length(currentChr) - maxOffset]
+				resultsThisGroup[[row]] <- resultsThisGroup[[row]] + tail(currentChr, 1) + (overlap[2*maxOffset+2] - overlap[2*maxOffset+1])
+				currentChr <- c(currentChr, resultsThisGroup[[row]])
 			}
 		}
-		#B vector for nnls
-		b <- rfData[indices]
-		#Values of 0.5 result in infinite estimated distance, which doesn't really work. 
-		b[b == 0.5] <- 0.49
-		result <- nnls::nnls(d, mapFunction(b)) 
-		map[[as.character(group)]] <- c(0, cumsum(result$x[which(indices[,1] == indices[,2]+1)]))
-		names(map[[as.character(group)]]) <- rfData@markers
+		map[[as.character(group)]] <- currentChr
 	}
 	class(map) <- "map"
 	return(map)
+}
+estimateMapInternal <- function(subsettedRF, maxOffset, verbose, mapFunction)
+{
+	if(verbose) 
+	{
+		cat("Generating design matrx\n", sep="")
+	}
+	#Construct design matrix
+	d <- .Call("generateDesignMatrix", length(subsettedRF@markers)-1, maxOffset, PACKAGE="mpMap2")
+	indices <- matrix(nrow=maxOffset * length(subsettedRF@markers) - maxOffset * (maxOffset + 1) / 2, ncol = 2)
+	counter <- 1
+	for(offset in 1:maxOffset)
+	{
+		for(i in 1:(length(subsettedRF@markers)-offset))
+		{
+			indices[counter,] <- c(i+offset, i)
+			counter <- counter + 1
+		}
+	}
+	#B vector for nnls
+	b <- subsettedRF[indices]
+	#Values of 0.5 result in infinite estimated distance, which doesn't really work. 
+	b[b == 0.5] <- 0.49
+	if(verbose) cat("Solving non-negative least squares problem\n", sep="")
+	result <- nnls::nnls(d, mapFunction(b))
+	results <- c(0, cumsum(result$x[which(indices[,1] == indices[,2]+1)]))
+	names(results) <- subsettedRF@markers
+	return(results)
 }
 designMat <- function(n, maxOffset)
 {
