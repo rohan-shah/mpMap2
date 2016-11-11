fcdd209613abeea075da28c73f8bbbee75e12761
Rohan Shah <rohan.shah@bookshah.com>
2016-11-11 13:24:28 +1100

    Added 4-parent test for imputeFounders with extra positions

--- tests/testthat/test-imputeFoundersExtraPositions.R
+++ tests/testthat/test-imputeFoundersExtraPositions.R
@@ -52,3 +52,32 @@ test_that("Test that imputations at positions look right for F2 populations",
 		indices <- which(result@geneticData[[1]]@imputed@data[,20] == 2 & result@geneticData[[1]]@imputed@data[,26] == 2)
 		expect_equal(as.integer(result@geneticData[[1]]@imputed@data[indices,c("P1", "P2", "P3", "P4", "P5")]), rep(2L, length(indices)*5))
 	})
+test_that("Test that imputations at positions look right for 4-way population using a single funnel",
+	{
+		map <- sim.map(len = 100, n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
+		map[[1]] <- map[[1]][c(1:20, 80:101)]
+		pedigree <- fourParentPedigreeSingleFunnel(initialPopulationSize = 1000, selfingGenerations = 1, intercrossingGenerations = 0)
+		pedigree@selfing <- "finite"
+
+		cross <- simulateMPCross(map=map, pedigree=pedigree, mapFunction = haldane)
+		mapped <- new("mpcrossMapped", cross, map = map)
+		suppressWarnings(result <- imputeFounders(mapped, errorProb = 0, extraPositions = list("1" = c("P1" = 30, "P2" = 40, "P3" = 50, "P4" = 60, "P5" = 70))))
+
+		#Certain different homozygotes should be seperated by hetrozygotes - E.g. (1, 3), (1, 4), (2, 3), (2, 4). 
+		indices <- which((result@geneticData[[1]]@imputed@data[,20] == 1 | result@geneticData[[1]]@imputed@data[,20] == 2) & (result@geneticData[[1]]@imputed@data[,26] == 3 | result@geneticData[[1]]@imputed@data[,26] == 4))
+		indices <- c(indices, which((result@geneticData[[1]]@imputed@data[,20] == 3 | result@geneticData[[1]]@imputed@data[,20] == 4) & (result@geneticData[[1]]@imputed@data[,26] == 1 | result@geneticData[[1]]@imputed@data[,26] == 2)))
+		#Check that everything between is imputed as a hetrozygote
+		expect_equal(as.logical(result@geneticData[[1]]@imputed@data[indices,c("P1", "P2", "P3", "P4", "P5")] >= 5), rep(TRUE, length(indices)*5))
+
+		#But some hetrozygotes are impossible in this pedigree. In that case the differente homozygotes are NOT seperated by a run of hets. 
+		indices <- which((result@geneticData[[1]]@imputed@data[,20] == 1 & result@geneticData[[1]]@imputed@data[,26] == 2) | (result@geneticData[[1]]@imputed@data[,20] == 2 & result@geneticData[[1]]@imputed@data[,26] == 1))
+		indices <- c(indices, which((result@geneticData[[1]]@imputed@data[,20] == 3 & result@geneticData[[1]]@imputed@data[,26] == 4) | (result@geneticData[[1]]@imputed@data[,20] == 4 & result@geneticData[[1]]@imputed@data[,26] == 3)))
+		#Check that everything between is imputed as a hetrozygote
+		expect_equal(as.logical(result@geneticData[[1]]@imputed@data[indices,c("P1", "P2", "P3", "P4", "P5")] <= 4), rep(TRUE, length(indices)*5))
+
+		#The same homozygote
+		indices <- which(result@geneticData[[1]]@imputed@data[,20] <= 4 & result@geneticData[[1]]@imputed@data[,26] <= 4 & result@geneticData[[1]]@imputed@data[,20] == result@geneticData[[1]]@imputed@data[,26])
+		#Check that everything between two identical homozygotes is imputed as the same homozygote
+		expectedValue <- as.integer(do.call(rbind, lapply(as.list(result@geneticData[[1]]@imputed@data[indices,20]), function(x) rep(x, 5L))))
+		expect_equal(as.integer(result@geneticData[[1]]@imputed@data[indices,c("P1", "P2", "P3", "P4", "P5")]), expectedValue)
+	})
