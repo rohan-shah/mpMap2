a188c2f649beb3500c57bbf6c09c05c8f82d1003
Rohan Shah <rohan.shah@bookshah.com>
2016-11-11 13:00:12 +1100

    Added extra test for imputeFounders with extra positions, using an F2 population.

--- tests/testthat/test-imputeFoundersExtraPositions.R
+++ tests/testthat/test-imputeFoundersExtraPositions.R
@@ -27,3 +27,28 @@ test_that("Test that the positions are put in the correct order",
 		expect_equal(names[index-1], "D2M61")
 		expect_equal(names[index+1], "D2M62")
 	})
+test_that("Test that imputations at positions look right for F2 populations",
+	{
+		map <- sim.map(len = 100, n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
+		map[[1]] <- map[[1]][c(1:20, 80:101)]
+		pedigree <- f2Pedigree(1000)
+		pedigree@selfing <- "finite"
+
+		cross <- simulateMPCross(map=map, pedigree=pedigree, mapFunction = haldane)
+		mapped <- new("mpcrossMapped", cross, map = map)
+		suppressWarnings(result <- imputeFounders(mapped, errorProb = 0, extraPositions = list("1" = c("P1" = 30, "P2" = 40, "P3" = 50, "P4" = 60, "P5" = 70))))
+
+		#Different homozygotes
+		indices <- which(result@geneticData[[1]]@imputed@data[,20] == 1 & result@geneticData[[1]]@imputed@data[,26] == 2)
+		indices <- c(which(result@geneticData[[1]]@imputed@data[,20] == 2 & result@geneticData[[1]]@imputed@data[,26] == 1))
+
+		#Check that everything between two homozygotes is imputed as a hetrozygote. 
+		expect_equal(as.integer(result@geneticData[[1]]@imputed@data[indices,c("P1", "P2", "P3", "P4", "P5")]), rep(3L, length(indices)*5))
+
+		#Check that everything between two identical homozygotes is imputed the same as the end-points
+		indices <- which(result@geneticData[[1]]@imputed@data[,20] == 1 & result@geneticData[[1]]@imputed@data[,26] == 1)
+		expect_equal(as.integer(result@geneticData[[1]]@imputed@data[indices,c("P1", "P2", "P3", "P4", "P5")]), rep(1L, length(indices)*5))
+
+		indices <- which(result@geneticData[[1]]@imputed@data[,20] == 2 & result@geneticData[[1]]@imputed@data[,26] == 2)
+		expect_equal(as.integer(result@geneticData[[1]]@imputed@data[indices,c("P1", "P2", "P3", "P4", "P5")]), rep(2L, length(indices)*5))
+	})
