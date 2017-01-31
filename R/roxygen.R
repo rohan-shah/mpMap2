47c97c82811eb92845c3f8bba160dafff0833275
Rohan Shah <rohan.shah@bookshah.com>
2017-02-01 00:15:26 +1100

    Changes to ensure R CMD check passes.

--- R/roxygen.R
+++ R/roxygen.R
@@ -1,9 +1,13 @@
 #' @import qtl
 #' @import igraph
 #' @import methods
+#' @import utils
+#' @import stats
+#' @import ggplot2
 #' @importFrom stats as.dist cutree pchisq rnorm
 #' @importFrom utils combn head tail
 #' @importFrom pryr address
+#' @importFrom fastcluster hclust
 #' @importFrom nnls nnls
 #' @exportClass pedigreeGraph
 #' @exportMethod subset 
--- tests/testthat/test-arsaRaw.R
+++ tests/testthat/test-arsaRaw.R
@@ -1,20 +1,23 @@
 context("Test multithreading of arsaRaw")
-test_that("Test that correct ordering is generated for an F2 population", 
-	{
-		f2Pedigree <- f2Pedigree(1000)
-		map <- sim.map(len = 100, n.mar = 101, anchor.tel=TRUE, include.x=FALSE, eq.spacing=TRUE)
-		cross <- simulateMPCross(map=map, pedigree=f2Pedigree, mapFunction = haldane)
-		cross <- subset(cross, markers = sample(1:101))
-		rf <- estimateRF(cross)
-		grouped <- formGroups(rf, groups = 1, method = "average", clusterBy = "theta")
+if(class(try(.Call("omp_set_num_threads", 1, PACKAGE="mpMap2"))) != "try-error")
+{
+	test_that("Test that correct ordering is generated for an F2 population", 
+		{
+			f2Pedigree <- f2Pedigree(1000)
+			map <- sim.map(len = 100, n.mar = 101, anchor.tel=TRUE, include.x=FALSE, eq.spacing=TRUE)
+			cross <- simulateMPCross(map=map, pedigree=f2Pedigree, mapFunction = haldane)
+			cross <- subset(cross, markers = sample(1:101))
+			rf <- estimateRF(cross)
+			grouped <- formGroups(rf, groups = 1, method = "average", clusterBy = "theta")
 
-		.Call("omp_set_num_threads", 1, PACKAGE="mpMap2")
-		orderedSingleThreaded <- orderCross(grouped)
-		.Call("omp_set_num_threads", 16, PACKAGE="mpMap2")
-		orderedMultiThreaded <- orderCross(grouped)
-		
-		correlationMultiThreaded <- cor(match(names(map[[1]]), markers(orderedMultiThreaded)), 1:101)
-		correlationSingleThreaded <- cor(match(names(map[[1]]), markers(orderedSingleThreaded)), 1:101)
-		expect_equal(abs(correlationMultiThreaded), 1, tolerance = 1e-1)
-		expect_equal(abs(correlationSingleThreaded), 1, tolerance = 1e-1)
-	})
+			.Call("omp_set_num_threads", 1, PACKAGE="mpMap2")
+			orderedSingleThreaded <- orderCross(grouped)
+			.Call("omp_set_num_threads", 16, PACKAGE="mpMap2")
+			orderedMultiThreaded <- orderCross(grouped)
+			
+			correlationMultiThreaded <- cor(match(names(map[[1]]), markers(orderedMultiThreaded)), 1:101)
+			correlationSingleThreaded <- cor(match(names(map[[1]]), markers(orderedSingleThreaded)), 1:101)
+			expect_equal(abs(correlationMultiThreaded), 1, tolerance = 1e-1)
+			expect_equal(abs(correlationSingleThreaded), 1, tolerance = 1e-1)
+		})
+}
