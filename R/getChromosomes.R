a56066b8d3929785a09f0d5d4a1e3b9b19412fb6
Rohan Shah <rohan.shah@bookshah.com>
2017-02-13 11:14:58 +1100

    Added function getChromosomes, to get chromosomes for a list of markers.

---
+++ R/getChromosomes.R
@@ -0,0 +1,14 @@
+#' @export
+getChromosomes <- function(mpcrossMapped, markers)
+{
+	isNewMpcrossMappedArgument(mpcrossMapped)
+	if(missing(markers) || !is.character(markers) || any(is.na(markers)))
+	{
+		stop("Input markers must be a character vector, without missing values")
+	}
+	chromosomeIndices <- unlist(sapply(markers, function(marker)
+	{
+		which(unlist(lapply(mpcrossMapped@map, function(x) !is.na(match(marker, names(x))))))
+	}))
+	return(names(mpcrossMapped@map)[chromosomeIndices])
+}
