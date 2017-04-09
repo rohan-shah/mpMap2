d325de93d8810797a233b6b4c163565bbd266773
Rohan Shah <rohan.shah@bookshah.com>
2017-04-09 12:39:08 +1000

    Added a listCodingErrorsMpMap function

--- R/listCodingErrors.R
+++ R/listCodingErrors.R
@@ -7,3 +7,30 @@ listCodingErrors <- function(founders, finals, hetData)
 	errors$null <- errors$null + 1
 	return(errors)
 }
+#' @export
+listCodingErrorsMpMap <- function(founders, finals)
+{
+	newHetDataList <- lapply(as.list(1:ncol(founders)), function(x)
+	{
+		if(any(is.na(founders[,x])))
+		{
+			finals[,x] <<- NA
+			return(matrix(0L, 0, 3))
+		}
+		else
+		{
+			uniqueAlleles <- unique(founders[, x])
+			retVal <- cbind(uniqueAlleles, uniqueAlleles, uniqueAlleles)
+			colnames(retVal) <- NULL
+			return(retVal)
+		}
+	})
+	names(newHetDataList) <- colnames(founders)
+
+	newHetData <- new("hetData", newHetDataList)
+	errors <- .Call("listCodingErrors", founders, finals, newHetData, PACKAGE="mpMap2")
+	errors$finals <- errors$finals + 1
+	errors$hetData <- errors$hetData + 1
+	errors$null <- errors$null + 1
+	return(errors)
+}
--- R/mpcross.R
+++ R/mpcross.R
@@ -51,21 +51,21 @@ fromMpMap <- function(mpcross, selfing = "infinite", fixCodingErrors = FALSE)
   names(newHetDataList) <- foundersMarkerNames
 
   newHetData <- new("hetData", newHetDataList)
+  codingErrors <- listCodingErrors(founders = mpcross$founders, finals = mpcross$finals, hetData = newHetData)
   if(fixCodingErrors)
   {
-    codingErrors <- listCodingErrors(founders = mpcross$founders, finals = mpcross$finals, hetData = newHetData)
     uniqueMarkers <- unique(codingErrors$finals[,"Column"])
     if(length(uniqueMarkers) > 0)
     {
       mpcross$finals[, uniqueMarkers] <- NA
       warning(paste0("Removing data for ", length(uniqueMarkers), " markers, because fixCodingErrors = TRUE was specified, and these markers had invalid alleles. For less aggressive removal, use listCodingErrors"))
     }
-    if(length(codingErrors$null))
-    {
-      newHetData[codingErrors$null] <- list(matrix(0L, 0, 3))
-      mpcross$finals[,codingErrors$null] <- NA
-      warning(paste0("Removing data for ", length(codingErrors$null), " markers, because fixCodingErrors = TRUE was specified, and a founder had an NA allele for these markers. For less aggressive removal, use listCodingErrors"))
-    }
+  }
+  if(length(codingErrors$null))
+  {
+    newHetData[codingErrors$null] <- list(matrix(0L, 0, 3))
+    mpcross$finals[,codingErrors$null] <- NA
+    warning(paste0("Removing data for ", length(codingErrors$null), " markers, because these markers have NA founder alleles"))
   }
   geneticData <- new("geneticData", finals = mpcross$finals, founders = mpcross$founders, pedigree = newPedigree, hetData = newHetData)
   geneticDataList <- new("geneticDataList", list(geneticData))
