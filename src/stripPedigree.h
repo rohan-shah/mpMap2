c8a26b7de7b32018921dd2b283aeee5a73874717
Rohan Shah <rohan.shah@bookshah.com>
2017-05-12 13:40:29 +1000

    Minor changes.

--- src/stripPedigree.h
+++ src/stripPedigree.h
@@ -1,5 +1,7 @@
 #ifndef STRIP_PEDIGREE_HEADER_GUARD_MPMAP2
 #define STRIP_PEDIGREE_HEADER_GUARD_MPMAP2
 #include <Rcpp.h>
+#ifdef USE_BOOST
 SEXP stripPedigree(SEXP pedigree_sexp, SEXP finalLines_sexp);
+#endif
 #endif
