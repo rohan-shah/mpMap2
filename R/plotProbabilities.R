aff7f714c5d804aae6cd4a1d631a00f162beb631
Rohan Shah <rohan.shah@bookshah.com>
2017-05-30 15:50:19 +1000

    Correct names on probabilities plot, when using finite generations of selfing. Also allow to subset which genotypes are shown. Typically this would be 1:nFounders.

--- R/plotProbabilities.R
+++ R/plotProbabilities.R
@@ -1,5 +1,5 @@
 #' @export
-plotProbabilities <- function(inputObject, positions)
+plotProbabilities <- function(inputObject, positions, alleles)
 {
 	if(inherits(inputObject, "mpcrossMapped"))
 	{
@@ -32,32 +32,41 @@ plotProbabilities <- function(inputObject, positions)
 	transposed <- transposeProbabilities(geneticData)
 	nLines <- nrow(transposed)
 	nFounders <- nFounders(geneticData)
+	
 	offsetVector <- c(0, head(unlist(lapply(geneticData@probabilities@map, max)), -1))
 	cumsumOffsetVector <- cumsum(offsetVector)
 	offsetVector <- rep(cumsumOffsetVector, times = unlist(lapply(geneticData@probabilities@map, length)))
 	if(geneticData@pedigree@selfing == "infinite")
 	{
+		if(missing(alleles)) alleles <- 1:8
 		dataSets <- list()
 		for(founder in 1:nFounders)
 		{
 			averages <- apply(transposed[,(0:(nPositions-1))*nFounders + founder], 2, mean)
 			dataSets[[founder]] <- data.frame(value = averages, positionName = positionNames, position = positions + offsetVector, chromosome = rep(names(geneticData@probabilities@map), times = unlist(lapply(geneticData@probabilities@map, length))), founder = rownames(founders(geneticData))[founder], stringsAsFactors = FALSE)
 		}
-		combined <- do.call(rbind, dataSets)
+		combined <- do.call(rbind, dataSets[alleles])
 		combined$founder <- factor(combined$founder)
 		lineData <- data.frame(position = tail(cumsumOffsetVector, -1))
 		ggplot(combined, aes(position, value, colour = founder)) + geom_line() + xlab("Distance (cM)") + ylab("Probability") + geom_vline(data = lineData, mapping = aes(xintercept = position)) + facet_wrap(~ founder, ncol = 1) + theme(legend.position="none")
 	}
 	else if(geneticData@pedigree@selfing == "finite")
 	{
+		if(missing(alleles)) alleles <- 1:nAlleles
 		dataSets <- list()
 		nAlleles <- length(unique(geneticData@probabilities@key[,3]))
 		for(allele in 1:nAlleles)
 		{
 			averages <- apply(transposed[,(0:(nPositions-1))*nAlleles + allele], 2, mean)
-			dataSets[[allele]] <- data.frame(value = averages, positionName = positionNames, position = positions + offsetVector, chromosome = rep(names(geneticData@probabilities@map), times = unlist(lapply(geneticData@probabilities@map, length))), allele = allele, stringsAsFactors = FALSE)
+			if(allele <= nFounders) alleleName <- rownames(founders(geneticData))[allele]
+			else
+			{
+				legendRow <- match(allele, geneticData@probabilities@key[,3])
+				alleleName <- paste0(rownames(founders(geneticData))[geneticData@probabilities@key[legendRow, 1]], " - ", rownames(founders(geneticData))[geneticData@probabilities@key[legendRow, 2]])
+			}
+			dataSets[[allele]] <- data.frame(value = averages, positionName = positionNames, position = positions + offsetVector, chromosome = rep(names(geneticData@probabilities@map), times = unlist(lapply(geneticData@probabilities@map, length))), allele = alleleName, stringsAsFactors = FALSE)
 		}
-		combined <- do.call(rbind, dataSets)
+		combined <- do.call(rbind, dataSets[alleles])
 		combined$allele <- factor(combined$allele)
 		lineData <- data.frame(position = tail(cumsumOffsetVector, -1))
 		ggplot(combined, aes(position, value, colour = allele)) + geom_line() + xlab("Distance (cM)") + ylab("Probability") + geom_vline(data = lineData, mapping = aes(xintercept = position)) + facet_wrap(~ allele, ncol = 1) + theme(legend.position="none")
