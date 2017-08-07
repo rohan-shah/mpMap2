6b05e837671e5983836ed4982f7ae2f274ad615e
Rohan Shah <rohan.shah@bookshah.com>
2017-08-08 08:50:57 +1000

    change exportMapToPretzl, so we can break up chromosomes, avoiding the file size limit.

--- R/exportMapToPretzl.R
+++ R/exportMapToPretzl.R
@@ -1,5 +1,5 @@
 #' @export
-exportMapToPretzl <- function(inputObject, name)
+exportMapToPretzl <- function(inputObject, name, separateChromosomes = FALSE)
 {
 	if(inherits(inputObject, "mpcrossMapped"))
 	{
@@ -13,18 +13,40 @@ exportMapToPretzl <- function(inputObject, name)
 	{
 		stop("inputObject must have class mpcrossMapped or map")
 	}
-	#Entry markers in the final json object
-	output <- lapply(map, function(x)
+	if(separateChromosomes)
 	{
-		data.frame(name = names(x), position = as.vector(x), stringsAsFactors = FALSE)
-	})
-	#Entry chromosomes in the final json object
-	chromosomes <- data.frame(name = names(map), stringsAsFactors = FALSE)
-	chromosomes$markers <- output
-	#Entry geneticMap in the final json object
-	output <- list(chromosomes = chromosomes, name = name)
-	#Final json object
-	output <- list(geneticmap = output)
-	toJson <- jsonlite::toJSON(output)
-	return(toJson)
+		#Entry markers in the final json object
+		toJSON <- lapply(1:length(map), function(x)
+		{
+			markerData <- list(data.frame(name = names(map[[x]]), position = as.vector(map[[x]]), stringsAsFactors = FALSE))
+			names(markerData) <- names(map)[x]
+			#Entry chromosomes in the final json object
+			chromosomes <- data.frame(name = names(map)[x], stringsAsFactors = FALSE)
+			chromosomes$markers <- markerData
+			#Entry geneticMap in the final json object
+			output <- list(chromosomes = chromosomes, name = paste0(name, "_", names(map)[x]))
+			#Final json object
+			output <- list(geneticmap = output)
+			return(jsonlite::toJSON(output))
+		})
+		names(toJSON) <- names(map)
+		return(toJSON)
+	}
+	else
+	{
+		#Entry markers in the final json object
+		output <- lapply(map, function(x)
+		{
+			data.frame(name = names(x), position = as.vector(x), stringsAsFactors = FALSE)
+		})
+		#Entry chromosomes in the final json object
+		chromosomes <- data.frame(name = names(map), stringsAsFactors = FALSE)
+		chromosomes$markers <- output
+		#Entry geneticMap in the final json object
+		output <- list(chromosomes = chromosomes, name = name)
+		#Final json object
+		output <- list(geneticmap = output)
+		toJson <- jsonlite::toJSON(output)
+		return(toJson)
+	}
 }
