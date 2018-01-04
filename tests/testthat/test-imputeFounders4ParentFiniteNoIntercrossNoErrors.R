5434222adf27f5325adc8f0fab772e6140abaa14
Rohan Shah <rohan.shah@bookshah.com>
2018-01-04 17:12:56 +1100

    Added tests that would catch the previous bug.

--- tests/testthat/test-imputeFounders4ParentFiniteNoIntercrossNoErrors.R
+++ tests/testthat/test-imputeFounders4ParentFiniteNoIntercrossNoErrors.R
@@ -1,5 +1,48 @@
 context("Founder imputation, four parents, finite selfing, no intercrossing, no errors")
-test_that("Test zero generations of intercrossing, with marker heterozygotes",
+test_that("Test zero generations of intercrossing, with marker heterozygotes, without selfing",
+	{
+		testFunc <- function(pedigree, map)
+		{
+			cross <- simulateMPCross(map=map, pedigree=pedigree, mapFunction = haldane)
+			mapped <- new("mpcrossMapped", cross, map = map)
+			suppressWarnings(result <- imputeFounders(mapped, errorProb = 0))
+
+			#Hetrozygotes will be discarded in imputation, which means that the imputed version won't be EXACTLY the same as the original data
+			expect_identical(result@geneticData[[1]]@imputed@data, result@geneticData[[1]]@finals)
+
+			cross2 <- cross + multiparentSNP(keepHets=TRUE)
+			mapped <- new("mpcrossMapped", cross2, map = map)
+			result <- imputeFounders(mapped, errorProb = 0)
+			tmp <- table(result@geneticData[[1]]@imputed@data, cross@geneticData[[1]]@finals)
+	
+			expect_identical(nrow(tmp), ncol(tmp))
+			expect_identical(rownames(tmp), colnames(tmp))
+
+			endHet <- nrow(tmp)
+			expect_gt(sum(diag(tmp)) / sum(tmp), 0.965)
+
+			expect_true(all(result@geneticData[[1]]@imputed@errors == 0))
+		}
+		map1 <- sim.map(len = 100, n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
+		map2 <- sim.map(len = c(100, 100), n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
+		maps <- list(map1, map2)
+
+		pedigree1 <- fourParentPedigreeSingleFunnel(initialPopulationSize = 500, selfingGenerations = 0, nSeeds = 1, intercrossingGenerations = 0)
+		pedigree1@selfing <- "finite"
+		pedigree2 <- fourParentPedigreeRandomFunnels(initialPopulationSize = 500, selfingGenerations = 0, nSeeds = 1, intercrossingGenerations = 0)
+		pedigree2@selfing <- "finite"
+
+		pedigrees <- list(pedigree1, pedigree2)
+		for(map in maps)
+		{
+			for(pedigree in pedigrees)
+			{
+				testFunc(pedigree, map)
+			}
+		}
+
+	})
+test_that("Test zero generations of intercrossing, with marker heterozygotes, with selfing",
 	{
 		testFunc <- function(pedigree, map)
 		{
@@ -52,7 +95,48 @@ test_that("Test zero generations of intercrossing, with marker heterozygotes",
 		}
 
 	})
-test_that("Test zero generations of intercrossing, without marker heterozygotes",
+test_that("Test zero generations of intercrossing, without marker heterozygotes, without selfing",
+	{
+		testFunc <- function(pedigree, map)
+		{
+			cross <- simulateMPCross(map=map, pedigree=pedigree, mapFunction = haldane)
+			mapped <- new("mpcrossMapped", cross, map = map)
+			suppressWarnings(result <- imputeFounders(mapped, errorProb = 0))
+
+			#Hetrozygotes will be discarded in imputation, which means that the imputed version won't be EXACTLY the same as the original data
+			expect_identical(result@geneticData[[1]]@imputed@data, result@geneticData[[1]]@finals)
+
+			cross2 <- cross + multiparentSNP(keepHets=FALSE)
+			mapped <- new("mpcrossMapped", cross2, map = map)
+			result <- imputeFounders(mapped, heterozygoteMissingProb = 1, homozygoteMissingProb = 0.01, errorProb = 0)
+			tmp <- table(result@geneticData[[1]]@imputed@data, cross@geneticData[[1]]@finals)
+			#Correct imputation rate should be 0.945
+			expect_gt(sum(diag(tmp)) / sum(tmp), 0.945)
+			expect_identical(nrow(tmp), ncol(tmp))
+			expect_identical(rownames(tmp), colnames(tmp))
+
+			expect_true(all(result@geneticData[[1]]@imputed@errors == 0))
+		}
+		map1 <- sim.map(len = 100, n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
+		map2 <- sim.map(len = c(100, 100), n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
+		maps <- list(map1, map2)
+
+		pedigree1 <- fourParentPedigreeSingleFunnel(initialPopulationSize = 500, selfingGenerations = 0, nSeeds = 1, intercrossingGenerations = 0)
+		pedigree1@selfing <- "finite"
+		pedigree2 <- fourParentPedigreeRandomFunnels(initialPopulationSize = 500, selfingGenerations = 0, nSeeds = 1, intercrossingGenerations = 0)
+		pedigree2@selfing <- "finite"
+
+		pedigrees <- list(pedigree1, pedigree2)
+		for(map in maps)
+		{
+			for(pedigree in pedigrees)
+			{
+				testFunc(pedigree, map)
+			}
+		}
+
+	})
+test_that("Test zero generations of intercrossing, without marker heterozygotes, with selfing",
 	{
 		testFunc <- function(pedigree, map)
 		{
--- tests/testthat/test-imputeFounders4ParentFiniteNoIntercrossWithErrors.R
+++ tests/testthat/test-imputeFounders4ParentFiniteNoIntercrossWithErrors.R
@@ -1,5 +1,49 @@
 context("Founder imputation, four parents, finite selfing, no intercrossing, with errors")
-test_that("Test zero generations of intercrossing, with marker heterozygotes",
+test_that("Test zero generations of intercrossing, with marker heterozygotes, without selfing",
+	{
+		testFunc <- function(pedigree, map)
+		{
+			cross <- simulateMPCross(map=map, pedigree=pedigree, mapFunction = haldane)
+			mapped <- new("mpcrossMapped", cross, map = map)
+			suppressWarnings(result <- imputeFounders(mapped, errorProb = 0.01))
+
+			tmp <- table(result@geneticData[[1]]@imputed@data, cross@geneticData[[1]]@finals)
+			expect_gt(sum(diag(tmp)) / sum(tmp), 0.98)
+
+			cross2 <- cross + multiparentSNP(keepHets=TRUE)
+			mapped <- new("mpcrossMapped", cross2, map = map)
+			result <- imputeFounders(mapped, errorProb = 0.01)
+			tmp <- table(result@geneticData[[1]]@imputed@data, cross@geneticData[[1]]@finals)
+	
+			expect_identical(nrow(tmp), ncol(tmp))
+			expect_identical(rownames(tmp), colnames(tmp))
+
+			endHet <- nrow(tmp)
+			expect_gt(sum(diag(tmp)) / sum(tmp), 0.965)
+
+			errors <- result@geneticData[[1]]@imputed@errors
+			expect_lt(sum(errors) / length(errors), 0.01)
+		}
+		map1 <- sim.map(len = 100, n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
+		map2 <- sim.map(len = c(100, 100), n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
+		maps <- list(map1, map2)
+
+		pedigree1 <- fourParentPedigreeSingleFunnel(initialPopulationSize = 500, selfingGenerations = 0, nSeeds = 1, intercrossingGenerations = 0)
+		pedigree1@selfing <- "finite"
+		pedigree2 <- fourParentPedigreeRandomFunnels(initialPopulationSize = 500, selfingGenerations = 0, nSeeds = 1, intercrossingGenerations = 0)
+		pedigree2@selfing <- "finite"
+
+		pedigrees <- list(pedigree1, pedigree2)
+		for(map in maps)
+		{
+			for(pedigree in pedigrees)
+			{
+				testFunc(pedigree, map)
+			}
+		}
+
+	})
+test_that("Test zero generations of intercrossing, with marker heterozygotes, with selfing",
 	{
 		testFunc <- function(pedigree, map)
 		{
@@ -53,7 +97,49 @@ test_that("Test zero generations of intercrossing, with marker heterozygotes",
 		}
 
 	})
-test_that("Test zero generations of intercrossing, without marker heterozygotes",
+test_that("Test zero generations of intercrossing, without marker heterozygotes, without selfing",
+	{
+		testFunc <- function(pedigree, map)
+		{
+			cross <- simulateMPCross(map=map, pedigree=pedigree, mapFunction = haldane)
+			mapped <- new("mpcrossMapped", cross, map = map)
+			suppressWarnings(result <- imputeFounders(mapped, errorProb = 0.01))
+
+			tmp <- table(result@geneticData[[1]]@imputed@data, cross@geneticData[[1]]@finals)
+			expect_gt(sum(diag(tmp)) / sum(tmp), 0.98)
+
+			cross2 <- cross + multiparentSNP(keepHets=FALSE)
+			mapped <- new("mpcrossMapped", cross2, map = map)
+			result <- imputeFounders(mapped, heterozygoteMissingProb = 1, homozygoteMissingProb = 0.01, errorProb = 0.01)
+			tmp <- table(result@geneticData[[1]]@imputed@data, cross@geneticData[[1]]@finals)
+			#Correct imputation rate should be 0.96
+			expect_gt(sum(diag(tmp)) / sum(tmp), 0.96)
+			expect_identical(nrow(tmp), ncol(tmp))
+			expect_identical(rownames(tmp), colnames(tmp))
+
+			errors <- result@geneticData[[1]]@imputed@errors
+			expect_lt(sum(errors) / length(errors), 0.01)
+		}
+		map1 <- sim.map(len = 100, n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
+		map2 <- sim.map(len = c(100, 100), n.mar = 101, anchor.tel = TRUE, include.x=FALSE, eq.spacing=TRUE)
+		maps <- list(map1, map2)
+
+		pedigree1 <- fourParentPedigreeSingleFunnel(initialPopulationSize = 500, selfingGenerations = 0, nSeeds = 1, intercrossingGenerations = 0)
+		pedigree1@selfing <- "finite"
+		pedigree2 <- fourParentPedigreeRandomFunnels(initialPopulationSize = 500, selfingGenerations = 0, nSeeds = 1, intercrossingGenerations = 0)
+		pedigree2@selfing <- "finite"
+
+		pedigrees <- list(pedigree1, pedigree2)
+		for(map in maps)
+		{
+			for(pedigree in pedigrees)
+			{
+				testFunc(pedigree, map)
+			}
+		}
+
+	})
+test_that("Test zero generations of intercrossing, without marker heterozygotes, with selfing",
 	{
 		testFunc <- function(pedigree, map)
 		{
@@ -104,5 +190,4 @@ test_that("Test zero generations of intercrossing, without marker heterozygotes"
 		}
 
 	})
-
 
