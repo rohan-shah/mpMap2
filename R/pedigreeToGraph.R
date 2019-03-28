2eb6fe0e49d1ce2ea36e8b1739c61a3ee9a4b3b4
Rohan Shah <rohan.shah@bookshah.com>
2019-03-28 14:21:55 +1100

    Added edge types (maternal / paternal) to pedigree graph

--- R/pedigreeToGraph.R
+++ R/pedigreeToGraph.R
@@ -3,17 +3,35 @@ pedigreeToGraph <- function(pedigree)
 {
 	founders <- nFounders(pedigree)
 	nPedigreeLines <- length(pedigree@lineNames)
-	motherEdges <- sapply((founders+1):nPedigreeLines, function(x) c(pedigree@lineNames[pedigree@mother[x]], pedigree@lineNames[x]))
-	#We don't want to repeat edges if there's selfing
+
+	motherEdges <- sapply((founders+1):nPedigreeLines, 
+		function(x) 
+		{
+			if(pedigree@mother[x] == pedigree@father[x]) return(c())
+			else c(pedigree@lineNames[pedigree@mother[x]], pedigree@lineNames[x])
+		})
 	fatherEdges <- sapply((founders+1):nPedigreeLines, 
 		function(x)
 		{
 			if(pedigree@mother[x] == pedigree@father[x]) return(c())
 			else return(c(pedigree@lineNames[pedigree@father[x]], pedigree@lineNames[x]))
 		})
-	#uneven lengths makes fatherEdges a list
+	selfingEdges <- sapply((founders+1):nPedigreeLines, 
+		function(x)
+		{
+			if(pedigree@mother[x] == pedigree@father[x]) return(c(pedigree@lineNames[pedigree@father[x]], pedigree@lineNames[x]))
+			else return(c())
+		})
+	
+	#uneven lengths make these lists
 	fatherEdges <- unlist(fatherEdges)
-	graph <- graph.empty() + vertices(pedigree@lineNames) + edges(c(motherEdges, fatherEdges))
+	motherEdges <- unlist(motherEdges)
+	selfingEdges <- unlist(selfingEdges)
+
+	graph <- graph.empty() + vertices(pedigree@lineNames) + edges(c(motherEdges, fatherEdges, selfingEdges))
+	type <- c(rep("maternal", length(motherEdges)/2), rep("paternal", length(fatherEdges)/2), rep("selfing", length(selfingEdges)/2))
+	E(graph)$type <- type
+
 	laidOut <- layout.reingold.tilford(graph, root = pedigree@lineNames[1:founders])
 	return(new("pedigreeGraph", graph = graph, layout = laidOut))
 }
